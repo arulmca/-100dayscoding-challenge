cfb94084a8d8f3a33aa141ede09ad402bf0e7bbf
Arulkumar Krishnan <>
2022-07-05 07:18:27 +0000

    print statement

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ height = int(input("What is your height in cm? "))
 if height >= 120:
   print("you can enjoy rollercoaster ride!")
 else:
-  print("Sorry, you are not eligible for the ride")
+  print("Sorry, you are not eligible for the rides")
